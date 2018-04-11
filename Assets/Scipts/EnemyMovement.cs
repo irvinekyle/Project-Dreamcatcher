8b7d2fcb11d1a9213978f48594efba82663e3d1f
irvinekyle <kyle.irvine007@gmail.com>
2018-04-11 21:00:40 +0200

    Enemy update
    - added in basic enemy health
    - added in basic enemy follow logic
    - smoother navmesh
    - player health

--- Assets/Scipts/EnemyMovement.cs
+++ Assets/Scipts/EnemyMovement.cs
@@ -5,22 +5,23 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour {
     Transform player;               // Reference to the player's position.
     //PlayerHealth playerHealth;      // Reference to the player's health.
-    //EnemyHealth enemyHealth;        // Reference to this enemy's health.
+    EnemyHealth enemyHealth;        // Reference to this enemy's health.
     NavMeshAgent nav;               // Reference to the nav mesh agent.
+    public float distToPlayer;       //stores the distance to player
 
 
     void Awake() {
         // Set up the references.
         player = GameObject.FindGameObjectWithTag("Player").transform;
         //playerHealth = player.GetComponent<PlayerHealth>();
-        //enemyHealth = GetComponent<EnemyHealth>();
         nav = GetComponent<NavMeshAgent>();
+        enemyHealth = GetComponent<EnemyHealth>();
     }
 
 
     void Update() {
         // If the enemy and the player have health left...
-        /*
+        
         if (enemyHealth.currentHealth > 0) {
             // ... set the destination of the nav mesh agent to the player.
             nav.SetDestination(player.position);
@@ -29,8 +30,19 @@ public class EnemyMovement : MonoBehaviour {
         else {
             // ... disable the nav mesh agent.
             nav.enabled = false;
-        }*/
+        }
         nav.SetDestination(player.position);
-        GetComponent<Animator>().Play("move_forward");
+        distToPlayer = Vector3.Distance(transform.position, player.transform.position);
+        if(distToPlayer <= 2.0 && enemyHealth.currentHealth > 0) {
+            GetComponent<Animator>().Play("attack_short_001");
+        }
+        else if (distToPlayer >= 30.0 && enemyHealth.currentHealth > 0) {
+            GetComponent<Animator>().Play("move_forward_fast");
+            nav.speed = nav.speed + 3.0f;
+        }
+        else if(distToPlayer >= 5.0 && enemyHealth.currentHealth > 0) {
+            GetComponent<Animator>().Play("move_forward");
+            nav.speed = 3.5f;
+        }
     }
 }
